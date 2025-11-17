312c3ede22c47164155fd5c8f1336a695e7e1045
k-e-i3 <143052502+k-e-i3@users.noreply.github.com>
2025-11-17 22:46:36 +0900

    Update data_r3_selective.js

--- data/data_r3_selective.js
+++ data/data_r3_selective.js
@@ -1,4 +1,4 @@
-const window.questions_r3_selective = [
+window.questions_r3_selective = [
   // ----------------------------------------
   // --- 令和3年 選択問題 No.2 ---
   // ----------------------------------------
