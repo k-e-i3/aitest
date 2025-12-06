e1a19efc05dd90a5a20959f8f19c84c410656ee8
k-e-i3 <143052502+k-e-i3@users.noreply.github.com>
2025-12-06 17:48:52 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 // ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
 // ここに Google AI Studio で取得したAPIキーを貼り付けてください
-const API_KEY = 'AIzaSyAjxxEkBy29LJon2onzQD8RlZTgBYDlHCc';
+const API_KEY = 'AIzaSyAr8fzPAD6rgthPcYETB9jKBqZAZWfPMd0';
 // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
